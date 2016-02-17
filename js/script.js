b720afcfda58962edcb2fd1dcc92bea2d2da5a35
Sambhuti <sambhuti.prakash@gmail.com>
2016-02-17 19:37:35 +0530

    Revert "-Animation added using CSS"
    This reverts commit add8ed32a52db88e986b5631163dc6b099dac818.

--- js/script.js
+++ js/script.js
@@ -29,41 +29,13 @@ $(document).ready(function() {
 });
 
 function setBindings() {
-    $("div a").click(function(e) {
-        e.preventDefault();
+    $("nav a").click(function(e) {
         var sectionId = e.currentTarget.id + "Section";
        
-        $("html body").animate({
-            scrollTop: $("#"+sectionId).offset().top}, 1000)
+        $('html,body').animate({
+            scrollTop: $("#" + sectionId).offset.top   
+            
+        }, 1000)
         
-    })
+    });
 }
-
-
-//Scroll Animation
-
-$(document).ready(function() { 
-
-
-	$(window).scroll(function(event) {
-
-		var y = $(this).scrollTop();
-
-		if (y >= 1000 && y<=2000) {
-
-			$('#phone').addClass('animate');
-			$('#tablet').addClass('animate');
-
-		}
-        
-       else
-           $('#phone').removeClass('animate')+$('#tablet').removeClass('animate');;
-			
-
-        
- 
-
-	});
-
-
-});
